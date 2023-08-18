d76ad08eb3f24d7c450124599e595ba8b41ab9c7
Alejandro Mendoza <zippi180@gmail.com>
2023-08-18 00:47:09 -0600

    Add default code for string interpolation exercise

---
+++ GitExercisesPlayground.playground/Pages/StringInterpolation.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+// Create a constant representing Mexico City elevation
+
+
+
+// Use string interpolation to print the following message to the console "Mexico City has an elevation of 2240 m"
